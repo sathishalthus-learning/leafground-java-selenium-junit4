3acfb82a70f77d5596a8695e869b5b58344506ee
sathishalthus-github <sathishalthus@hotmail.com>
2023-11-15 22:44:39 +0530

    ABC Finalized.

--- src/test/java/leafground/AbcOfLeafGround.java
+++ src/test/java/leafground/AbcOfLeafGround.java
@@ -1,7 +1,37 @@
 package leafground;
 
+import java.time.Duration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AbcOfLeafGround {
 	
+	WebDriver driver;
 	
+	@Before
+	public void beforeTest() throws InterruptedException {
+		//
+		driver=new ChromeDriver();
+		driver.get("https://www.leafground.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Thread.sleep(Duration.ofSeconds(3));
+		
+	}
+	
+	@After
+	public void afterTest() throws InterruptedException {
+		Thread.sleep(Duration.ofSeconds(3));
+		driver.quit();
+		
+	}
 
+	@Test
+	public void basicTest() {
+		System.out.println("testleaf - testground");
+	}
 }
